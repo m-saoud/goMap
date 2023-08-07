f956f25c57c689aa5f867dc7ca427a426709f458
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-08-07 19:29:05 +0900

    fetch API

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-let map;
+// GOOGLE MAPS
+let map, bounds;
+const API = "AIzaSyBcghr5VKE4JyNEcKG390FbFA2r_C7vsos";
+const init_center = { lat: 35.689487, lng: 139.691711 };
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("mapcounter"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function initMap() {
+  map = new google.maps.Map(document.getElementById("mapContainer"), {
+    center: init_center,
+    zoom: 11,
+    disableDefaultUI: true,
   });
+  bounds = new google.maps.LatLngBounds();
 }
+window.initMap = initMap;
 
-initMap();
+const fetchCountry = async () => {
+  const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "4525616425msh3fb92284d1f0298p124236jsn01e6e586e6b8",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+// fetchCountry();
+
+const fetchHotels = async () => {
+  const url = "https://hotels4.p.rapidapi.com/v2/get-meta-data";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "4525616425msh3fb92284d1f0298p124236jsn01e6e586e6b8",
+      "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchHotels();
+(async function loadingContries() {
+  const countries = await fetchCountry();
+  const contryList = document.querySelector("#countries_list");
+
+  for (let i = 0; i < countries.length; i++) {
+    console.log = countries[i].name;
+    contryList.innerHTML += `<li>${countries[i].name}</li>`;
+  }
+})();
